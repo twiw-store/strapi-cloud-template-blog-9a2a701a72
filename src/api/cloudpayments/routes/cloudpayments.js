5bc1775478d2484dd4bcc2adbdc334a180fcced3
Eduard <twiw1925@gmail.com>
2025-12-14 14:03:44 +0700

    fix: allow GET+POST for cloudpayments webhooks

--- src/api/cloudpayments/routes/cloudpayments.js
+++ src/api/cloudpayments/routes/cloudpayments.js
@@ -1,28 +1,21 @@
-export default {
+'use strict';
+
+module.exports = {
   routes: [
-    {
-      method: 'POST',
-      path: '/payments/cloudpayments/check',
-      handler: 'cloudpayments.check',
-      config: { auth: false },
-    },
-    {
-      method: 'POST',
-      path: '/payments/cloudpayments/pay',
-      handler: 'cloudpayments.pay',
-      config: { auth: false },
-    },
-    {
-      method: 'POST',
-      path: '/payments/cloudpayments/confirm',
-      handler: 'cloudpayments.confirm',
-      config: { auth: false },
-    },
-    {
-      method: 'POST',
-      path: '/payments/cloudpayments/fail',
-      handler: 'cloudpayments.fail',
-      config: { auth: false },
-    },
+    // CHECK
+    { method: 'GET',  path: '/payments/cloudpayments/check', handler: 'cloudpayments.check', config: { auth: false } },
+    { method: 'POST', path: '/payments/cloudpayments/check', handler: 'cloudpayments.check', config: { auth: false } },
+
+    // PAY
+    { method: 'GET',  path: '/payments/cloudpayments/pay', handler: 'cloudpayments.pay', config: { auth: false } },
+    { method: 'POST', path: '/payments/cloudpayments/pay', handler: 'cloudpayments.pay', config: { auth: false } },
+
+    // CONFIRM
+    { method: 'GET',  path: '/payments/cloudpayments/confirm', handler: 'cloudpayments.confirm', config: { auth: false } },
+    { method: 'POST', path: '/payments/cloudpayments/confirm', handler: 'cloudpayments.confirm', config: { auth: false } },
+
+    // FAIL
+    { method: 'GET',  path: '/payments/cloudpayments/fail', handler: 'cloudpayments.fail', config: { auth: false } },
+    { method: 'POST', path: '/payments/cloudpayments/fail', handler: 'cloudpayments.fail', config: { auth: false } },
   ],
 };
