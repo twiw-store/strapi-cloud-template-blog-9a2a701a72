42aaf43acc2cc7c3cfd8f421dbff4df22fd504da
Eduard <twiw1925@gmail.com>
2025-12-14 15:35:43 +0700

    test: add unique cp ping route

--- src/api/cloudpayments/routes/cloudpayments.js
+++ src/api/cloudpayments/routes/cloudpayments.js
@@ -4,27 +4,14 @@ module.exports = {
   routes: [
     {
       method: 'POST',
-      path: '/cloudpayments/check',
+      path: '/__cp_ping_45182db',
       handler: 'cloudpayments.check',
       config: { auth: false },
     },
-    {
-      method: 'POST',
-      path: '/cloudpayments/pay',
-      handler: 'cloudpayments.pay',
-      config: { auth: false },
-    },
-    {
-      method: 'POST',
-      path: '/cloudpayments/confirm',
-      handler: 'cloudpayments.confirm',
-      config: { auth: false },
-    },
-    {
-      method: 'POST',
-      path: '/cloudpayments/fail',
-      handler: 'cloudpayments.fail',
-      config: { auth: false },
-    },
+
+    { method: 'POST', path: '/cloudpayments/check', handler: 'cloudpayments.check', config: { auth: false } },
+    { method: 'POST', path: '/cloudpayments/pay', handler: 'cloudpayments.pay', config: { auth: false } },
+    { method: 'POST', path: '/cloudpayments/confirm', handler: 'cloudpayments.confirm', config: { auth: false } },
+    { method: 'POST', path: '/cloudpayments/fail', handler: 'cloudpayments.fail', config: { auth: false } },
   ],
 };
