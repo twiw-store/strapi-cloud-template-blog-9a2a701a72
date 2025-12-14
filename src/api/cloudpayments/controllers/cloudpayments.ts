caae0f74b2964a8d0068a382110012139f8e9b12
Eduard <twiw1925@gmail.com>
2025-12-14 16:32:26 +0700

    feat: cloudpayments webhooks (ts)

---
+++ src/api/cloudpayments/controllers/cloudpayments.ts
@@ -0,0 +1,17 @@
+export default {
+  async check(ctx) {
+    ctx.body = { code: 0 };
+  },
+
+  async pay(ctx) {
+    ctx.body = { code: 0 };
+  },
+
+  async confirm(ctx) {
+    ctx.body = { code: 0 };
+  },
+
+  async fail(ctx) {
+    ctx.body = { code: 0 };
+  },
+};
